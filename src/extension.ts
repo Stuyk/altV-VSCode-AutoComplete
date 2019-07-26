f184b788a3960c67b757aab4a58b1918e08fa2ca
Stuyk <stuykgaming@gmail.com>
2019-07-25 21:12:56 -0600

    Native Update

--- src/extension.ts
+++ src/extension.ts
@@ -12,6 +12,7 @@ import altServerPositionJSON from './json/alt-server-position.json';
 import altServerPlayerJSON from './json/alt-server-player.json';
 import altWeatherIdsJSON from './json/alt-weather-ids.json';
 import altServerVehicleJSON from './json/alt-server-vehicle.json';
+import altClientNativesJSON from './json/alt-client-natives.json';
 
 export function activate(context: vscode.ExtensionContext) {
 	vscode.window.showInformationMessage('alt:V auto complete has loaded.');
@@ -39,5 +40,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// alt-server-vehicle.json
 	context.subscriptions.push(
 		snippetHelper.loadSnippets('altservervehicle', altServerVehicleJSON, ['veh'])
+	);
+
+	// alt-client-natives.json
+	context.subscriptions.push(
+		snippetHelper.loadSnippets('altclientnatives', altClientNativesJSON, ['native'])
 	);
 }
