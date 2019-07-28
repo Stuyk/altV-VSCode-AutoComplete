e5f4c921ace148dfafa4cd53848b77a8819d8679
Tim <tim-holz1@web.de>
2019-07-28 16:39:32 +0200

    added chat auto complete

--- src/extension.ts
+++ src/extension.ts
@@ -13,6 +13,7 @@ import altServerPlayerJSON from './json/alt-server-player.json';
 import altWeatherIdsJSON from './json/alt-weather-ids.json';
 import altServerVehicleJSON from './json/alt-server-vehicle.json';
 import altClientNativesJSON from './json/alt-client-natives.json';
+import altChatJSON from './json/alt-chat.json';
 
 export function activate(context: vscode.ExtensionContext) {
 	vscode.window.showInformationMessage('alt:V auto complete has loaded.');
@@ -45,5 +46,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// alt-client-natives.json
 	context.subscriptions.push(
 		snippetHelper.loadSnippets('altclientnatives', altClientNativesJSON, ['native'])
+	);
+
+	// alt-chat.json
+	context.subscriptions.push(
+		snippetHelper.loadSnippets('altchat', altChatJSON, ['chat'])
 	);
 }
