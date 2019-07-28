d0bd8f70dab50ac95c135c751a725a5dc1acfa27
Stuyk <stuykgaming@gmail.com>
2019-07-28 17:40:28 -0600

    Fixed prefixes and added client placeholders

--- src/extension.ts
+++ src/extension.ts
@@ -14,42 +14,48 @@ import altWeatherIdsJSON from './json/alt-weather-ids.json';
 import altServerVehicleJSON from './json/alt-server-vehicle.json';
 import altClientNativesJSON from './json/alt-client-natives.json';
 import altChatJSON from './json/alt-chat.json';
+import altClientJSON from './json/alt-client.json';
 
 export function activate(context: vscode.ExtensionContext) {
 	vscode.window.showInformationMessage('alt:V auto complete has loaded.');
 
 	// alt-server.json
 	context.subscriptions.push(
-		snippetHelper.loadSnippets('altserver', altServerJSON, ['alts'])
+		snippetHelper.loadSnippets('altserver', altServerJSON, ['server_'])
 	);
 	
 	// alt-server-position.json
 	context.subscriptions.push(
-		snippetHelper.loadSnippets('altserverposition', altServerPositionJSON, ['pos'])
+		snippetHelper.loadSnippets('altserverposition', altServerPositionJSON, ['pos_'])
 	);
 
 	// alt-server-player.json
 	context.subscriptions.push(
-		snippetHelper.loadSnippets('altserverplayer', altServerPlayerJSON, ['player'])
+		snippetHelper.loadSnippets('altserverplayer', altServerPlayerJSON, ['player_'])
 	);
 
 	// alt-weather-ids.json
 	context.subscriptions.push(
-		snippetHelper.loadSnippets('altweatherids', altWeatherIdsJSON, ['weather'])
+		snippetHelper.loadSnippets('altweatherids', altWeatherIdsJSON, ['weather_'])
 	);
 
 	// alt-server-vehicle.json
 	context.subscriptions.push(
-		snippetHelper.loadSnippets('altservervehicle', altServerVehicleJSON, ['veh'])
+		snippetHelper.loadSnippets('altservervehicle', altServerVehicleJSON, ['vehicle_'])
 	);
 
 	// alt-client-natives.json
 	context.subscriptions.push(
-		snippetHelper.loadSnippets('altclientnatives', altClientNativesJSON, ['native'])
+		snippetHelper.loadSnippets('altclientnatives', altClientNativesJSON, ['native_'])
 	);
 
 	// alt-chat.json
 	context.subscriptions.push(
-		snippetHelper.loadSnippets('altchat', altChatJSON, ['chat'])
+		snippetHelper.loadSnippets('altchat', altChatJSON, ['chat_'])
+	);
+
+	// alt-client.json
+	context.subscriptions.push(
+		snippetHelper.loadSnippets('altclient', altClientJSON, ['client_'])
 	);
 }
